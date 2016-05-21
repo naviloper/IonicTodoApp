41ff4d9728ebb4bd60f11ccd8bfe282d42e75058
Navid Hosseini <navid045@gmail.com>
2016-05-22 02:14:47 +0430

    Some sample code added to home page

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -1,11 +1,30 @@
-import {Page} from 'ionic-angular';
+import {Page, NavController} from 'ionic-angular';
 
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
+
+  static get parameters(){
+    return [[NavController]]
+  }
+
+  constructor(nav) {
+    this.nav = nav;
+
+    this.items = [
+      {title: 'hi1', description: 'test1'},
+      {title: 'hi2', description: 'test2'},
+      {title: 'hi3', description: 'test3'},
+    ];
+  }
+
+  addItem(){
+
+  }
+
+  viewItem(){
 
   }
 }
